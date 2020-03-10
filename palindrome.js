88b1db7b0c670db46c35eb1373d782b12319ec9d
manjusurya <manjusuryamsu@gmail.com>
2020-03-10 06:20:38 +0530

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+var x=232;
+var y=x;
+var rev=0;
+var rem=0;
+while (x>0) {
+    rem=x%10;
+    rev=rev*10+rem;
+    x=parseInt(x/10);
+}
+    if(rev==y)
+    {
+
+console.log("palindrome")
+}
+else{
+    console.log("not palindrome")
+}
--- while.js
+++ while.js
@@ -1,9 +1,10 @@
 var i=1;
 while (i<=100) {
-    if (i%2==0) 
+    if (i%2==0) {
         
     
     console.log(i);
-    i++
+    }
+    i++;
     
 }
